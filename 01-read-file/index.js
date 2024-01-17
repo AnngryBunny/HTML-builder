86c02ea4e8038f82d8c122a2c4667eb96a4ebe96
Hanna Shevchenko <sh9vch9nk0@gmail.com>
2024-01-17 22:13:08 +0200

    feat: add the solution for the 1 task (Wed, Jan 17, 2024 10:13:08 PM)

---
+++ 01-read-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// Solution 1
+
+/* const readStream = fs.createReadStream('01-read-file/text.txt');
+
+readStream.on('data', (chunk) => console.log(chunk.toString())); */
+
+// Solution 2
+
+const path = require('node:path');
+
+const textPath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(textPath);
+
+readStream.on('data', (chunk) => process.stdout.write(chunk.toString() + '\n'));
