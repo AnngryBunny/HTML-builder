dec5471cc216085e6b32dd398cc5db308117eba6
Hanna Shevchenko <sh9vch9nk0@gmail.com>
2024-01-23 01:41:22 +0200

    feat: start task 6 (Tue, Jan 23, 2024  1:41:22 AM)

---
+++ 06-build-page/index.js
@@ -0,0 +1,15 @@
+// Import all required modules
+
+// Read and save the template file in a variable
+
+// Find all tag names in the template file
+
+// Replace template tags with the content of component files
+
+// Write the modified template to the `index.html` file in the `project-dist` folder
+
+// Use the script written in task **05-merge-styles** to create the `style.css` file
+
+// Use the script from task **04-copy-directory** to move the `assets` folder into the `project-dist` folder
+
+// node 06-build-page
