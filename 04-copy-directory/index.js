5ff3a9b6b6d666d6809223acea84f4036cb7d447
Hanna Shevchenko <sh9vch9nk0@gmail.com>
2024-01-22 23:44:38 +0200

    feat: add a solution to the task 4 (Mon, Jan 22, 2024 11:44:38 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+// Import all required modules
+
+const fs = require('fs');
+const path = require('node:path');
+const { stdout } = process;
+const fsPromises = require('fs').promises;
+
+// Create the `files-copy` folder if it does not exist yet
+
+const filesCopyPath = path.join(__dirname, 'files-copy');
+
+async function createFolder() {
+  fs.readdir(__dirname, (err, files) => {
+    if (![...files].includes('files-copy')) {
+      fs.mkdir(filesCopyPath, (err) => {
+        if (err) throw err;
+        stdout.write('New folder was created');
+      });
+    } else {
+      stdout.write('This folder already exists');
+    }
+  });
+}
+
+// Read the contents of the `files` folder
+
+const filesPath = path.join(__dirname, 'files');
+
+const readFiles = () => {
+  fs.readdir(filesPath, (err, files) => {
+    [...files].forEach((file) => {
+      const originPath = path.join(filesPath, file);
+      const copyPath = path.join(filesCopyPath, file);
+      // Copy files from the `files` folder to the `files-copy` folder
+      return fsPromises.copyFile(originPath, copyPath);
+    });
+  });
+};
+
+createFolder().then(readFiles);
+
 // node 04-copy-directory
