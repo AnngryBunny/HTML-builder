eed34ae955a5d6d092ee9b134b3d138ae362ed14
Hanna Shevchenko <sh9vch9nk0@gmail.com>
2024-01-23 20:45:16 +0200

    feat: add first part of the solution task 6 (Tue, Jan 23, 2024  8:45:16 PM)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,12 +2,37 @@
 
 const fs = require('fs');
 const path = require('node:path');
+const fsPromises = require('fs').promises;
 
 // Read and save the template file in a variable
 
 const templatePath = path.join(__dirname, 'template.html');
+const componentsPath = path.join(__dirname, 'components');
+
+let content;
+let components;
+let componentsPaths = [];
+
+async function readTemplate() {
+  content = await fsPromises.readFile(templatePath, 'utf-8');
+  return content;
+}
 
 // Replace template tags with the content of component files
+
+async function readComponents() {
+  components = await fsPromises.readdir(componentsPath);
+  return components;
+}
+
+async function createComponentPath() {
+  for (let component of components) {
+    const componentPath = path.join(__dirname, 'components', component);
+    componentsPaths.push(componentPath);
+  }
+}
+
+readTemplate().then(readComponents).then(createComponentPath);
 
 // Write the modified template to the `index.html` file in the `project-dist` folder
 
