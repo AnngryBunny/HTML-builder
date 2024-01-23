67ebfb3e6c283593a9a9adb7b74f9271e5d25e15
Hanna Shevchenko <sh9vch9nk0@gmail.com>
2024-01-24 00:23:21 +0200

    feat: add index.html task 6 (Wed, Jan 24, 2024 12:23:21 AM)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,17 +1,30 @@
 // Import all required modules
 
-const fs = require('fs');
 const path = require('node:path');
 const fsPromises = require('fs').promises;
 
-// Read and save the template file in a variable
+// Paths
 
 const templatePath = path.join(__dirname, 'template.html');
 const componentsPath = path.join(__dirname, 'components');
+const indexPath = path.join(__dirname, 'project-dist', 'index.html');
+
+// Variables
 
 let content;
 let components;
-let componentsPaths = [];
+
+// Create 'project-dist' folder
+
+async function createFolder() {
+  const folders = await fsPromises.readdir(__dirname);
+
+  if (!folders.includes('project-dist')) {
+    fsPromises.mkdir(path.join(__dirname, 'project-dist'));
+  }
+}
+
+// Read and save the template file in a variable
 
 async function readTemplate() {
   content = await fsPromises.readFile(templatePath, 'utf-8');
@@ -25,19 +38,44 @@ async function readComponents() {
   return components;
 }
 
-async function createComponentPath() {
+async function replaceTags() {
   for (let component of components) {
-    const componentPath = path.join(__dirname, 'components', component);
-    componentsPaths.push(componentPath);
+    const extension = path.extname(component);
+    const basename = path.basename(component, extension);
+    const templateTag = `{{${basename}}}`;
+
+    const contentReader = await fsPromises.readFile(
+      path.join(__dirname, 'components', component),
+      'utf-8',
+    );
+
+    if (content.match(templateTag)) {
+      content = content.replace(templateTag, contentReader);
+    }
   }
 }
 
-readTemplate().then(readComponents).then(createComponentPath);
-
 // Write the modified template to the `index.html` file in the `project-dist` folder
+
+async function modifyIndexFile() {
+  fsPromises.writeFile(indexPath, content, {
+    encoding: 'utf8',
+    flag: 'w',
+  });
+}
 
 // Use the script written in task **05-merge-styles** to create the `style.css` file
 
 // Use the script from task **04-copy-directory** to move the `assets` folder into the `project-dist` folder
+
+// IIFE
+
+(async function () {
+  await createFolder();
+  await readTemplate();
+  await readComponents();
+  await replaceTags();
+  await modifyIndexFile();
+})();
 
 // node 06-build-page
